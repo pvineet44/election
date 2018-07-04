6bee85cdaa4b670972a56e7cc029b78289a4b78a
VINEET PAREKH <pvineet44@gmail.com>
2018-07-04 21:39:32 +0530

    Add candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,14 +1,31 @@
 pragma solidity ^0.4.2;
 
 contract Election {
-	//Store candidate
-	//Read candidate
-	string public candidate;
-	//Constructor
+		// Model a candidate
+		
+		struct Candidate{
+			uint id;
+			string name;
+			uint voteCount;
+		}
+
+		// Store candidate
+		// Fetch candidate 
+		mapping(uint => Candidate) public candidates; //Cant iterate/find length
+		//Store candidate count
+		uint public candidatesCount; //will have to explicitly keep a count of number of candidates
+
 
 		function Election() public {
 
-			candidate = "Modiji";
+			addCandidate("Modiji");
+			addCandidate("Rahul");
+
+		}
+
+		function addCandidate(string _name) private {
+			candidatesCount++;
+			candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);//0 for voteCount
 
 		}
 }

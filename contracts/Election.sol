c720576db6cfd3077d77c814b08ebd2721189ce0
VINEET PAREKH <pvineet44@gmail.com>
2018-07-05 08:11:51 +0530

    Done done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,12 +8,17 @@ contract Election {
 			string name;
 			uint voteCount;
 		}
-
+		//Store accounts that have voted
+		mapping(address => bool) public voters; 
 		// Store candidate
 		// Fetch candidate 
 		mapping(uint => Candidate) public candidates; //Cant iterate/find length
 		//Store candidate count
 		uint public candidatesCount; //will have to explicitly keep a count of number of candidates
+
+		event votedEvent (
+        uint indexed _candidateId
+    );
 
 
 		function Election() public {
@@ -27,5 +32,25 @@ contract Election {
 			candidatesCount++;
 			candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);//0 for voteCount
 
+		}
+
+		function vote (uint _candidateId) public {
+			//require that they haven't voted before
+
+			require(!voters[msg.sender]);
+
+			//require a valid candidate
+
+			require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+
+			//record that voter has voted
+			voters[msg.sender] = true;
+
+			//update candidate voteCount
+			candidates[_candidateId].voteCount++;
+
+			//trigger the votedEvent
+			votedEvent(_candidateId);
 		}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,7 @@ App = {
     return App.initWeb3();
   },
 
+//This basically connects our client side application to the local blockchain
   initWeb3: function() {
     if (typeof web3 !== 'undefined') {
       // If a web3 instance is already provided by Meta Mask.
@@ -19,58 +20,97 @@ App = {
     }
     return App.initContract();
   },
+initContract: function() {
+  $.getJSON("Election.json", function(election) {
+    // Instantiate a new truffle contract from the artifact
+    App.contracts.Election = TruffleContract(election);
+    // Connect provider to interact with contract
+    App.contracts.Election.setProvider(App.web3Provider);
 
-  initContract: function() {
-    $.getJSON("Election.json", function(election) {
-      // Instantiate a new truffle contract from the artifact
-      App.contracts.Election = TruffleContract(election);
-      // Connect provider to interact with contract
-      App.contracts.Election.setProvider(App.web3Provider);
+    App.listenForEvents();
 
-      return App.render();
+    return App.render();
+  });
+},
+
+
+  listenForEvents: function() {
+  App.contracts.Election.deployed().then(function(instance) {
+    instance.votedEvent({}, {
+      fromBlock: 0,
+      toBlock: 'latest'
+    }).watch(function(error, event) {
+      console.log("event triggered", event)
+      // Reload when a new vote is recorded
+      App.render();
     });
-  },
+  });
+},
 
   render: function() {
-    var electionInstance;
-    var loader = $("#loader");
-    var content = $("#content");
+  var electionInstance;
+  var loader = $("#loader");
+  var content = $("#content");
 
-    loader.show();
-    content.hide();
+  loader.show();
+  content.hide();
 
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
-      if (err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
-      }
-    });
+  // Load account data
+  web3.eth.getCoinbase(function(err, account) {
+    if (err === null) {
+      App.account = account;
+      $("#accountAddress").html("Your Account: " + account);
+    }
+  });
 
-    // Load contract data
+  // Load contract data
+  App.contracts.Election.deployed().then(function(instance) {
+    electionInstance = instance;
+    return electionInstance.candidatesCount();
+  }).then(function(candidatesCount) {
+    var candidatesResults = $("#candidatesResults");
+    candidatesResults.empty();
+
+    var candidatesSelect = $('#candidatesSelect');
+    candidatesSelect.empty();
+
+    for (var i = 1; i <= candidatesCount; i++) {
+      electionInstance.candidates(i).then(function(candidate) {
+        var id = candidate[0];
+        var name = candidate[1];
+        var voteCount = candidate[2];
+
+        // Render candidate Result
+        var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+        candidatesResults.append(candidateTemplate);
+
+        // Render candidate ballot option
+        var candidateOption = "<option value='" + id + "' >" + name + "</ option>"
+        candidatesSelect.append(candidateOption);
+      });
+    }
+    return electionInstance.voters(App.account);
+  }).then(function(hasVoted) {
+    // Do not allow a user to vote
+    if(hasVoted) {
+      $('form').hide();
+    }
+    loader.hide();
+    content.show();
+  }).catch(function(error) {
+    console.warn(error);
+  });
+},
+castVote: function() {
+    var candidateId = $('#candidatesSelect').val();
     App.contracts.Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidatesCount();
-    }).then(function(candidatesCount) {
-      var candidatesResults = $("#candidatesResults");
-      candidatesResults.empty();
-
-      for (var i = 1; i <= candidatesCount; i++) {
-        electionInstance.candidates(i).then(function(candidate) {
-          var id = candidate[0];
-          var name = candidate[1];
-          var voteCount = candidate[2];
-
-          // Render candidate Result
-          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
-          candidatesResults.append(candidateTemplate);
-        });
-      }
-
-      loader.hide();
-      content.show();
-    }).catch(function(error) {
-      console.warn(error);
+      return instance.vote(candidateId, { from: App.account });
+    }).then(function(result) {
+      // Wait for votes to update
+      $("#content").hide();
+      $("#loader").show();
+    }).catch(function(err) {
+      console.error(err);
     });
   }
 };
